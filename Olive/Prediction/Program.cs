a2d0f4f3280b1b3272f8f44de5d4ce93eec4ddfd
Dan Bednar <dan.bednar@montagetalent.com>
2021-05-08 14:04:58 -0500

    Shelling

---
+++ Olive/Prediction/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'predictAnswer' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY stockData
+     *  2. INTEGER_ARRAY queries
+     */
+
+    public static List<int> predictAnswer(List<int> stockData, List<int> queries) {
+
+    }
+
+}
+
+class Solution {
+    public static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter("C:\\Users\\DanBednar\\Documents\\GitHub\\Olive\\Olive\\Prediction\\test.txt", true);
+
+        int stockDataCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> stockData = new List<int>();
+
+        for (int i = 0; i < stockDataCount; i++) {
+            int stockDataItem = Convert.ToInt32(Console.ReadLine().Trim());
+            stockData.Add(stockDataItem);
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> queries = new List<int>();
+
+        for (int i = 0; i < queriesCount; i++) {
+            int queriesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            queries.Add(queriesItem);
+        }
+
+        List<int> result = Result.predictAnswer(stockData, queries);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Olive/Sort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'getEventsOrder' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING team1
+     *  2. STRING team2
+     *  3. STRING_ARRAY events1
+     *  4. STRING_ARRAY events2
+     */
+
+    public static List<string> getEventsOrder(string team1, string team2, List<string> events1, List<string> events2) {
+
+    }
+
+}
+
+class Solution {
+    public static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter("C:\\Users\\DanBednar\\Documents\\GitHub\\Olive\\Olive\\Sort\\test.txt", true);
+
+        string team1 = Console.ReadLine();
+
+        string team2 = Console.ReadLine();
+
+        int events1Count = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> events1 = new List<string>();
+
+        for (int i = 0; i < events1Count; i++) {
+            string events1Item = Console.ReadLine();
+            events1.Add(events1Item);
+        }
+
+        int events2Count = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> events2 = new List<string>();
+
+        for (int i = 0; i < events2Count; i++) {
+            string events2Item = Console.ReadLine();
+            events2.Add(events2Item);
+        }
+
+        List<string> result = Result.getEventsOrder(team1, team2, events1, events2);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
